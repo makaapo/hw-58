dc4b3e2a53b35670f01b56ae043f2af52b77fcb8
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-06-10 01:34:31 +0600

    Created level 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-const App = () => {
+import Modal from './components/Modal/Modal';
+import { useState } from 'react';
 
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
+      <button
+        className="w-25 btn btn-primary"
+        onClick={() => setShowModal(true)}
+      >
+        Order
+      </button>
+      <Modal show={showModal} title="Order" onClose={() => setShowModal(false)}>
+        <div className="modal-body">content</div>
+        <div className="modal-footer">
+          <button
+            className="btn btn-danger"
+            onClick={() => setShowModal(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
     </>
-  )
+  );
 };
 
-export default App
+export default App;
